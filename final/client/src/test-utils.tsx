f28b89bdf3a2a8eae9f6ece42189111251ab98cc
ayleen <ayleenchan@csu.fullerton.edu>
2021-11-17 16:20:13 -0800

    testing commit

--- final/client/src/test-utils.tsx
+++ final/client/src/test-utils.tsx
@@ -4,6 +4,13 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { MockedProvider, MockedResponse } from '@apollo/client/testing';
 
+// setup file
+import { configure } from 'enzyme';
+// @ts-ignore
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 type RenderApolloOptions = {
   mocks?: MockedResponse[],
   addTypename?: any,
