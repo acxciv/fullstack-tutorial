92533a93ce1225c97b609a4e8eb75a28668cca76
ayleen <ayleenchan@csu.fullerton.edu>
2021-11-17 16:27:49 -0800

    Testing Enzyme Config

--- final/client/src/test-utils.tsx
+++ final/client/src/test-utils.tsx
@@ -10,6 +10,7 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+//testing enzyme
 
 type RenderApolloOptions = {
   mocks?: MockedResponse[],
